e32d524b54886d41d262a7bc07501cd4801098d4
cryptorally72-alt <cryptorally72@gmail.com>
2026-08-18 13:51:26 +0300

    Update Staking.sol

--- Staking.sol
+++ Staking.sol
@@ -1,1 +1,16 @@
+/*
+Staking Contract
 
+Purpose:
+Allow users to stake tokens and earn rewards.
+
+Features:
+- Stake tokens
+- Unstake tokens
+- Reward calculations
+
+Future improvements:
+- Multiple pools
+- Bonus rewards
+- Governance incentives
+*/
