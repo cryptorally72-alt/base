44a50acf1d7a637ce9b5add1dae1d6fad4bd510f
cryptorally72-alt <cryptorally72@gmail.com>
2026-08-19 20:31:37 +0300

    Update MessageBoard.sol

--- MessageBoard.sol
+++ MessageBoard.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract MessageBoard {
+
+    string public message;
+
+    constructor() {
+        message = "Welcome to Based";
+    }
+
+    function updateMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
